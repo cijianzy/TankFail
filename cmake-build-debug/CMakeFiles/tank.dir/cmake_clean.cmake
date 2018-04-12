ce96f346cf304e89e1ba1f6f58ce548e4a0cb2bd
此间 <zhangyue.zhangyue@alibaba-inc.com>
2018-04-13 01:10:25 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/tank.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tank.dir/main.cpp.o"
+  "tank.pdb"
+  "tank"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
